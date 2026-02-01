ca02a4d18c49f7d2bb8598ce19c3db8919105664
raichu-CS <RaiChuCS@outlook.com>
2026-02-01 13:22:08 -0800

    Changed .java name to ClassName.java

---
+++ TEST2/GithubTest/javatest/ClassName.java
@@ -0,0 +1,10 @@
+public class ClassName {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int x = 1;
+        int y = 2;
+        int z = x + y;
+        System.out.println("Sum: " + z);
+    }
+}
